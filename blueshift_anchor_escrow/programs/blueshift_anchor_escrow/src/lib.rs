924d3c0c7813b20993e4bc11454d6cd34309ec83
AvhiMaz <avhimazumder5@outlook.com>
2025-05-28 18:56:25 +0530

    done with blueshift anchor escrow program

--- blueshift_anchor_escrow/programs/blueshift_anchor_escrow/src/lib.rs
+++ blueshift_anchor_escrow/programs/blueshift_anchor_escrow/src/lib.rs
@@ -1,16 +1,30 @@
+#![allow(unexpected_cfgs)]
 use anchor_lang::prelude::*;
 
-declare_id!("3qSEKwxFrZYLNhqufv9647bfZnJpxLHbQU7rL6pspMXw");
+mod errors;
+mod instructions;
+mod state;
+
+use instructions::*;
+
+declare_id!("22222222222222222222222222222222222222222222");
 
 #[program]
 pub mod blueshift_anchor_escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    #[instruction(discriminator = 0)]
+    pub fn make(ctx: Context<Make>, seed: u64, recieve: u64, amount: u64) -> Result<()> {
+        make::handler(ctx, seed, recieve, amount)
+    }
+
+    #[instruction(discriminator = 1)]
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        take::handler(ctx)
+    }
+
+    #[instruction(discriminator = 2)]
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        refund::handler(ctx)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
